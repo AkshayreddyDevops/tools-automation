9633a9e2d353376829f2e3cf42ebbd3eb341ac73
reddy.akshay.gaddam <reddy.akshay.gaddam@gmail.com>
2024-07-22 11:45:40 +0530

    E2E Application setup

--- infra/ec2.tf
+++ infra/ec2.tf
@@ -22,7 +22,7 @@ resource "aws_security_group" "sg" {
     to_port = 22
     protocol = "TCP"
     cidr_blocks = ["0.0.0.0/0"]
-    ipv6_blocks = ["::/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
   tags = {
     Name = "${var.name}-sg"
