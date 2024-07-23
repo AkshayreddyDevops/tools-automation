234762e57538c3497bd3a8ae1c6881f46b56b4ce
reddy.akshay.gaddam <reddy.akshay.gaddam@gmail.com>
2024-07-23 17:25:04 +0530

    E2E Application setup

--- infra/ec2.tf
+++ infra/ec2.tf
@@ -37,3 +37,11 @@ resource "aws_security_group" "sg" {
   }
 
 }
+
+# resource "aws_route53_record" "www" {
+#   zone_id = aws_route53_zone.primary.zone_id
+#   name    = "www.example.com"
+#   type    = "A"
+#   ttl     = 300
+#   records = [aws_eip.lb.public_ip]
+# }
