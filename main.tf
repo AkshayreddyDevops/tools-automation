23d25cd071193219ca4259aca6ad21b6ff70c0cb
reddy.akshay.gaddam <reddy.akshay.gaddam@gmail.com>
2024-07-26 21:53:47 +0530

    VPC creation

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
-# module "tool_infra_create"{
-#   for_each = var.tools
-#   source = "./infra"
-#   name = each.key
-#   instance_type=each.value["instance_type"]
-#   policy_name = each.value["policy_name"]
-#   hosted_zone = "Z0314836TZ55HM33F8PT"
-# }
+module "tool_infra_create"{
+  for_each = var.tools
+  source = "./infra"
+  name = each.key
+  instance_type=each.value["instance_type"]
+  policy_name = each.value["policy_name"]
+  hosted_zone = "Z0314836TZ55HM33F8PT"
+}
 
 
 module "vpc"{
